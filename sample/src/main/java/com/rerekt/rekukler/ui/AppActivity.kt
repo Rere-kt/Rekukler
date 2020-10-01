7e770df092a5db7be483666251039ad0ef0c16f9
airatlovesmusic <airat@rcsmail.ru>
2020-10-01 19:49:03 +0300

    [Sample] add MainFragment

--- sample/src/main/java/com/rerekt/rekukler/ui/AppActivity.kt
+++ sample/src/main/java/com/rerekt/rekukler/ui/AppActivity.kt
@@ -2,13 +2,21 @@ package com.rerekt.rekukler.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.rerekt.rekukler.R
 
-class AppActivity: AppCompatActivity() {
+open class AppActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) { goTo(MainFragment()) }
+    }
+
+    private fun goTo(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
 
 }
---
+++ sample/src/main/java/com/rerekt/rekukler/ui/MainFragment.kt
@@ -0,0 +1,14 @@
+package com.rerekt.rekukler.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.rerekt.rekukler.R
+
+class MainFragment: Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
