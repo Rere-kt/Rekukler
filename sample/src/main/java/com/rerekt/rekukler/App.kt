629733b4687c292b5e85dbe4f7b12d60868cc671
airatlovesmusic <airat@rcsmail.ru>
2020-10-01 19:44:32 +0300

    [Sample] Add first activity

---
+++ sample/src/main/java/com/rerekt/rekukler/App.kt
@@ -0,0 +1,11 @@
+package com.rerekt.rekukler
+
+import android.app.Application
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ sample/src/main/java/com/rerekt/rekukler/ui/AppActivity.kt
@@ -0,0 +1,14 @@
+package com.rerekt.rekukler.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rerekt.rekukler.R
+
+class AppActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
