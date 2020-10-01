d0a4f03b59763129edc3fad1631f8b159f6dce1c
airatlovesmusic <airat@rcsmail.ru>
2020-10-01 20:16:20 +0300

    [Sample] update sample project with new recyclerview dsl

--- sample/src/main/java/com/rerekt/rekukler/ui/MainFragment.kt
+++ sample/src/main/java/com/rerekt/rekukler/ui/MainFragment.kt
@@ -3,12 +3,26 @@ package com.rerekt.rekukler.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.rerekt.rekukler.R
+import com.rerekt.rekukler.configure
 
 class MainFragment: Fragment(R.layout.fragment_main) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler)
+        initRecycler(recyclerView)
+    }
+
+    private fun initRecycler(recyclerView: RecyclerView) {
+        recyclerView.configure {
+            linearLayout {
+                reverseLayout = false
+                orientation = LinearLayoutManager.VERTICAL
+            }
+        }
     }
 
 }
